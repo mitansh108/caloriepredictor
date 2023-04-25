e329aea48bb2affff31d08d5695840a5dc537d94
Mitansh Patel <mitanshpatel@Mitanshs-MacBook-Pro-4.local>
2023-04-25 23:19:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+
+from flask import Flask,render_template,request
+import numpy as np
+import xgboost
+import sklearn
+
+app = Flask(__name__)
+
+model = pickle.load(open('/Users/mitanshpatel/Downloads/calpred.pkl','rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict_cals():
+    int_features = [int(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    prediction = model.predict(features)
+
+    return str(prediction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8003)
